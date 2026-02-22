74fc436bc05a2892ac50dbb5772be1d697b32fac
David Huang <djhuang636@gmail.com>
2026-02-22 08:59:09 -0500

    complete test by running query to neverssl

--- eecs498-se/lec10-dns/getaddrinfo.cpp
+++ eecs498-se/lec10-dns/getaddrinfo.cpp
@@ -9,11 +9,13 @@
 //     ~ParsedUrl();
 // }
 
+#include <cassert>
 #include <iostream>
 #include <netdb.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -27,7 +29,8 @@ void PrintAddress(const sockaddr_in *s, const size_t saLength)
     cout << "Host address length = " << saLength << " bytes" << endl;
     cout << "Family = " << s->sin_family << ", port = " << ntohs(s->sin_port)
          << ", address = " << (a >> 24) << '.' << ((a >> 16) & 0xff) << '.'
-         << ((a >> 8) & 0xff) << '.' << (a & 0xff) << endl;
+         << ((a >> 8) & 0xff) << '.' << (a & 0xff) << '\n'
+         << endl;
 };
 
 int main()
@@ -45,4 +48,38 @@ int main()
     // getaddrinfo() call
     int getAddrResult = getaddrinfo("www.nytimes.com", "80", &hints, &address);
     PrintAddress((sockaddr_in *)address->ai_addr, sizeof(struct sockaddr_in));
+
+    // do something with the HTTP address.
+    // I don't do anything here since this is an example but in the real code
+    // you would take the address result and pass that into getMessage.
+    int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    assert(s != -1);
+
+    int connectResult = connect(s, address->ai_addr, address->ai_addrlen);
+    assert(connectResult == 0);
+
+    string getMessage;
+    getMessage = "GET / HTTP/1.1\r\n"
+                 "Host: neverssl.com\r\n"
+                 "User-Agent: LinuxGetUrl/2.0 twoogle498@umich.edu (Linux)\r\n"
+                 "Accept: */*\r\n"
+                 "Accept-Encoding: identity\r\n"
+                 "Connection: close\r\n"
+                 "\r\n";
+
+    // send the GET message to nytimes
+    send(s, getMessage.c_str(), getMessage.length(), 0);
+
+    // read the response
+    char buffer[10240];
+    int bytes;
+    while ((bytes = recv(s, buffer, sizeof(buffer), 0)) > 0)
+    {
+        write(1, buffer, bytes);
+    }
+
+    close(s);
+
+    // free address
+    freeaddrinfo(address);
 };
