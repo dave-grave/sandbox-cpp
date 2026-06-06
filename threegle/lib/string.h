1a07e5c39bab529ae590580e72c7963c8c38d254
David Huang <djhuang636@gmail.com>
2026-06-05 18:33:04 -0600

    started string lib. oops didn't meant to git reset

---
+++ threegle/lib/string.h
@@ -0,0 +1,191 @@
+// string.h
+
+// -- objectives -- 
+// 1. automatic tests CI
+// 2. insert() and erase() as according to STL
+// 3. bounds checking on indexing operations; throw an exception when accessing out of bounds.
+// 4. .find() methods to find the index or iterator to a given element in a vector or string.
+// 5. method to find the index or iterator to a substring within a given string.
+// 6. method to split a string based on a delimiter or predicate.
+// 7. method to join a vector of strings with a separator.
+// 8. impl. queue, stack, deque etc. data structures 
+
+#include <cstddef>   // for size_t
+#include <iostream>  // for ostream
+#include <cstring>  // for memcpy, strlen 
+#include <cstdlib>  // for malloc
+
+class string
+   {
+   public:  
+
+      // Default Constructor
+      // REQUIRES: Nothing
+      // MODIFIES: *this
+      // EFFECTS: Creates an empty string
+      string( ) 
+         {
+			 cstr_ = nullptr;
+			 size_ = 0;
+			 capacity_ = 0; // TODO: short string optimization
+         }
+
+      // string Literal / C string Constructor
+      // REQUIRES: cstr is a null terminated C style string
+      // MODIFIES: *this
+      // EFFECTS: Creates a string with equivalent contents to cstr
+      string( const char *cstr )
+         {
+			 // if (size_ = sizeof(cstr) - 1 < 16) 
+			 // {
+			 //     // TODO: short string optimization on stack
+			 //     cstr_ = cstr;
+			 // } 
+			 // else 
+				// heap allocation 
+				
+			 size_ = strlen(cstr);
+			 cstr_ = malloc(size_ + 1);
+			 if (cstr_ == NULL) 
+			 {
+				 // TODO: call custom exception
+				 return;
+			 }
+			 capacity_ = size_;
+			 memcpy(cstr_, cstr, size_ + 1); 
+         }
+		
+	  ~string( )
+	  {
+		  free(cstr_);
+	  }
+
+      // Size
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns the number of characters in the string
+      size_t size( ) const
+         {
+			 return size_;
+         }
+
+      // C string Conversion
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns a pointer to a null terminated C string of *this
+      const char *cstr( ) const
+         {
+			 return cstr_;
+         }
+
+      // Iterator Begin
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns a random access iterator to the start of the string
+      const char *begin( ) const
+         {
+			 return cstr_;
+         }
+
+      // Iterator End
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns a random access iterator to the end of the string
+      const char *end( ) const
+         {
+			 return cstr_ + size_;
+         }
+
+      // Element Access
+      // REQUIRES: 0 <= i < size()
+      // MODIFIES: Allows modification of the i'th element
+      // EFFECTS: Returns the i'th character of the string
+      char &operator [ ]( size_t i )
+         {
+         }
+
+      // string Append
+      // REQUIRES: Nothing
+      // MODIFIES: *this
+      // EFFECTS: Appends the contents of other to *this, resizing any
+      //      memory at most once
+      void operator+=( const string &other )
+         {
+         }
+
+      // Push Back
+      // REQUIRES: Nothing
+      // MODIFIES: *this
+      // EFFECTS: Appends c to the string
+      void pushBack( char c )
+         {
+         }
+
+      // Pop Back
+      // REQUIRES: string is not empty
+      // MODIFIES: *this
+      // EFFECTS: Removes the last charater of the string
+      void popBack( )
+         {
+         }
+
+      // Equality Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether all the contents of *this
+      //    and other are equal
+      bool operator==( const string &other ) const
+         {
+         }
+
+      // Not-Equality Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether at least one character differs between
+      //    *this and other
+      bool operator!=( const string &other ) const
+         {
+         }
+
+      // Less Than Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether *this is lexigraphically less than other
+      bool operator<( const string &other ) const
+         {
+         }
+
+      // Greater Than Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether *this is lexigraphically greater than other
+      bool operator>( const string &other ) const
+         {
+         }
+
+      // Less Than Or Equal Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether *this is lexigraphically less or equal to other
+      bool operator<=( const string &other ) const
+         {
+         }
+
+      // Greater Than Or Equal Operator
+      // REQUIRES: Nothing
+      // MODIFIES: Nothing
+      // EFFECTS: Returns whether *this is lexigraphically less or equal to other
+      bool operator>=( const string &other ) const
+         {
+         }
+
+   private:
+		const char* cstr_; 		// heap-allocated string
+		const char* sso_; 		// short string optimization on stack
+		size_t size_;
+		size_t capacity_;
+   };
+
+std::ostream &operator<<( std::ostream &os, const string &s )
+   {
+   }
